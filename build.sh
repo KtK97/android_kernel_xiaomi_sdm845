f0fca02652b91a4f238fc41d6aa3ca18a602e195
Bruno Martins <bgcngm@gmail.com>
2018-09-09 00:08:57 +0100

    dts: Rewrite notification LED configuration
    Signed-off-by: Park Ju Hyung <qkrwngud825@gmail.com>

--- build.sh
+++ build.sh
@@ -1,18 +1,38 @@
 #!/bin/bash
 rm -rf /out
 make kernelversion
-export ARCH=arm64 && export SUBARCH=arm64
 export USE_CCACHE=1
 export CCACHE_DIR=~/.ccache
 mkdir -p out
+echo
+echo "Cleaning OUT"
+echo
 make O=out clean
+echo
+echo "Running mrproper"
+echo
 make O=out mrproper
+echo
+echo "Making config file"
+echo
 make O=out ARCH=arm64 noodle_beryllium_defconfig
-PATH="~/kernel/toolchain/custom-clang/bin:~/kernel/toolchain/gcc9/bin:${PATH}"
-make -j$(nproc --all) O=out ARCH=arm64 CC=clang CLANG_TRIPLE=aarch64-linux-gnu- CROSS_COMPILE=aarch64-linux-gnu- CROSS_COMPILE_ARM32=arm-linux-androideabi-
+echo
+echo "Compiling the source"
+echo
+PATH="/home/ktk/kernel/toolchain/custom-clang/bin:/home/ktk/kernel/toolchain/gcc9/bin:${PATH}"
+make -j2 O=out ARCH=arm64 CC=clang CLANG_TRIPLE=aarch64-linux-gnu- CROSS_COMPILE=aarch64-linux-gnu- CROSS_COMPILE_ARM32=~/kernel/toolchain/gcc-32/bin/arm-linux-androideabi-
+echo
+echo "The image is compiled"
+echo
 mv out/arch/arm64/boot/Image.gz-dtb ~/kernel/anykernel2/Image.gz-dtb
 rm -rf out
 cd ~/kernel/anykernel2
+echo
+echo "Creating a Flashable ZIP"
+echo
 zip -r9 noodle_kernel-$(date +"%Y-%m-%d").zip *
 rm -rf Image.gz-dtb
+echo
+echo "Movied to ZIPs Folder "
+echo
 mv noodle_kernel-$(date +"%Y-%m-%d").zip ~/kernel/ZIPs
