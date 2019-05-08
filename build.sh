d6b2955c5e0026600223df4e8064b57626f8b7fc
KtK97 <guptakarthikay1997@gmail.com>
2019-05-08 21:47:51 +0530

    Script: Some changes

--- build.sh
+++ build.sh
@@ -9,7 +9,7 @@ make O=out clean
 make O=out mrproper
 make O=out ARCH=arm64 noodle_beryllium_defconfig
 PATH="~/kernel/toolchain/custom-clang/bin:~/kernel/toolchain/gcc9/bin:${PATH}" \
-make -j$(nproc --all) O=out ARCH=arm64 CC=clang CLANG_TRIPLE=aarch64-linux-gnu- CROSS_COMPILE=aarch64-linux-android-
+make -j$(nproc --all) O=out ARCH=arm64 CC=clang CLANG_TRIPLE=aarch64-linux-gnu- CROSS_COMPILE=aarch64-linux-gnu-
 
 mv out/arch/arm64/boot/Image.gz-dtb ~/kernel/anykernel/Image.gz-dtb
 rm -rf out
