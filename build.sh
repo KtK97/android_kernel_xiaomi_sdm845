3c0832a352e3c43337005e107fd7ba5f6a417f86
KtK97 <guptakarthikay1997@gmail.com>
2019-05-08 21:46:31 +0530

    Added custom build script

---
+++ build.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+rm -rf /out
+make kernelversion
+export ARCH=arm64 && export SUBARCH=arm64
+export USE_CCACHE=1
+export CCACHE_DIR=~/.ccache
+mkdir -p out
+make O=out clean
+make O=out mrproper
+make O=out ARCH=arm64 noodle_beryllium_defconfig
+PATH="~/kernel/toolchain/custom-clang/bin:~/kernel/toolchain/gcc9/bin:${PATH}" \
+make -j$(nproc --all) O=out ARCH=arm64 CC=clang CLANG_TRIPLE=aarch64-linux-gnu- CROSS_COMPILE=aarch64-linux-android-
+
+mv out/arch/arm64/boot/Image.gz-dtb ~/kernel/anykernel/Image.gz-dtb
+rm -rf out
+cd ~/kernel/anykernel
+zip -r noodle_kernel-$(date +"%Y-%m-%d").zip *
+rm -rf Image.gz-dtb
