c0a4ee528ef1141f66b421d377678976b70f7124
KtK97 <guptakarthikay1997@gmail.com>
2019-05-08 22:05:19 +0530

    Script: changes

--- build.sh
+++ build.sh
@@ -8,11 +8,11 @@ mkdir -p out
 make O=out clean
 make O=out mrproper
 make O=out ARCH=arm64 noodle_beryllium_defconfig
-PATH="~/kernel/toolchain/custom-clang/bin:~/kernel/toolchain/gcc9/bin:${PATH}" \
+PATH="~/kernel/toolchain/custom-clang/bin:~/kernel/toolchain/gcc9/bin:${PATH}"
 make -j$(nproc --all) O=out ARCH=arm64 CC=clang CLANG_TRIPLE=aarch64-linux-gnu- CROSS_COMPILE=aarch64-linux-gnu-
-
-mv out/arch/arm64/boot/Image.gz-dtb ~/kernel/anykernel/Image.gz-dtb
+mv out/arch/arm64/boot/Image.gz-dtb ~/kernel/anykernel2/Image.gz-dtb
 rm -rf out
-cd ~/kernel/anykernel
-zip -r noodle_kernel-$(date +"%Y-%m-%d").zip *
+cd ~/kernel/anykernel2
+zip -r9 noodle_kernel-$(date +"%Y-%m-%d").zip *
 rm -rf Image.gz-dtb
+mv noodle_kernel-$(date +"%Y-%m-%d").zip ~/kernel/ZIPs
